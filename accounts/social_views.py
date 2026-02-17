6f520e8cffebb254733508e1464fe6fdaafafccd
Atticus Ezis <atticus.ezis@gmail.com>
2026-02-18 00:43:41 +0900

    correct callback url

---
+++ accounts/social_views.py
@@ -0,0 +1,12 @@
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
+
+from resume_builder.settings import FRONTEND_DOMAIN
+
+
+class GoogleLoginView(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+    # frontend location for jwt tokens
+    callback_url = f"{FRONTEND_DOMAIN}/account/login/google-callback/"
+    client_class = OAuth2Client
