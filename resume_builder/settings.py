f1c04899765a2e9ddd44f6bc872fe8c7573c8b41
Atticus Ezis <atticus.ezis@gmail.com>
2026-02-18 18:04:39 +0900

    added logging for prod

--- resume_builder/settings.py
+++ resume_builder/settings.py
@@ -264,3 +264,17 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "ERROR",
+    },
+}
