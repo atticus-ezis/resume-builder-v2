9a3306935003f0f35eddb46a6e4d9088ffb8e16d
Atticus Ezis <atticus.ezis@gmail.com>
2026-02-20 15:34:03 +0900

    removed preload

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,4 @@ python create_superuser.py
 echo "Starting celery worker in background..."
 celery -A resume_builder worker -l info &
 echo "Starting gunicorn on 0.0.0.0:${PORT}..."
-exec gunicorn resume_builder.wsgi:application --bind "0.0.0.0:${PORT}" --workers "${GUNICORN_WORKERS:-1}" --timeout 120 --preload
+exec gunicorn resume_builder.wsgi:application --bind "0.0.0.0:${PORT}" --workers "${GUNICORN_WORKERS:-1}" --timeout 120
