06508818f425fe76c1604446e73a571ee88a9737
Atticus Ezis <atticus.ezis@gmail.com>
2026-02-20 20:00:25 +0900

    moved db call to post migrate

--- accounts/apps.py
+++ accounts/apps.py
@@ -5,14 +5,20 @@ class AccountsConfig(AppConfig):
     name = "accounts"
 
     def ready(self):
-        from django.conf import settings
-        from django.contrib.sites.models import Site
-        from django.db.utils import OperationalError, ProgrammingError
+        from django.db.models.signals import post_migrate
 
-        try:
-            Site.objects.update_or_create(
-                id=settings.SITE_ID,
-                defaults={"domain": settings.SITE_DOMAIN, "name": settings.SITE_NAME},
-            )
-        except (OperationalError, ProgrammingError):
-            pass
+        post_migrate.connect(_setup_site, sender=self)
+
+
+def _setup_site(sender, **kwargs):
+    from django.conf import settings
+    from django.contrib.sites.models import Site
+    from django.db.utils import OperationalError, ProgrammingError
+
+    try:
+        Site.objects.update_or_create(
+            id=settings.SITE_ID,
+            defaults={"domain": settings.SITE_DOMAIN, "name": settings.SITE_NAME},
+        )
+    except (OperationalError, ProgrammingError):
+        pass
