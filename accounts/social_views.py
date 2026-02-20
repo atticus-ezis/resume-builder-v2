066b5aff95108e04c9d05aadf46b87d94dc03a1b
Atticus Ezis <atticus.ezis@gmail.com>
2026-02-20 23:41:47 +0900

    fix import sattement

--- accounts/social_views.py
+++ accounts/social_views.py
@@ -1,16 +1,15 @@
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
+from django.conf import settings
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.permissions import AllowAny
 
-from resume_builder.settings import FRONTEND_DOMAIN
-
 
 class GoogleLoginView(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
-    callback_url = f"{FRONTEND_DOMAIN}/account/login/google-callback"
+    callback_url = f"{settings.FRONTEND_ORIGINS[0]}/account/login/google-callback"
     client_class = OAuth2Client
     permission_classes = [AllowAny]
     authentication_classes = []
