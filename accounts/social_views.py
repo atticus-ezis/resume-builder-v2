d4813ee2e7cbc0a8325bda4eb322f59fed05481f
Atticus Ezis <atticus.ezis@gmail.com>
2026-02-20 17:09:25 +0900

    made google singin csrf exempt

--- accounts/social_views.py
+++ accounts/social_views.py
@@ -1,6 +1,9 @@
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.permissions import AllowAny
 
 from resume_builder.settings import FRONTEND_DOMAIN
 
@@ -9,3 +12,9 @@ class GoogleLoginView(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
     callback_url = f"{FRONTEND_DOMAIN}/account/login/google-callback"
     client_class = OAuth2Client
+    permission_classes = [AllowAny]
+    authentication_classes = []
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
